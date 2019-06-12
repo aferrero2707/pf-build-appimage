fc80f10a513552d070871e37f9bdd5f0c3b6314f
aferrero2707 <aferrero1975@gmail.com>
2019-06-12 11:24:16 +0200

    Passing TRAVIS_BRANCH variable

--- build-docker.sh
+++ build-docker.sh
@@ -3,10 +3,10 @@
 export PF_BRANCH=stable
 #rm -rf RawTherapee
 if [ ! -e PhotoFlow ]; then
-	git clone https://github.com/aferrero2707/PhotoFlow.git --branch $PF_BRANCH --single-branch
+	git clone https://github.com/aferrero2707/PhotoFlow.git --branch $PF_BRANCH --depth=1 --single-branch
 fi
 rm -rf PhotoFlow/appimage
 mkdir -p PhotoFlow/appimage
 cp -a *.sh PhotoFlow/appimage
 cd PhotoFlow
-docker run -it -v $(pwd):/sources -e "PF_BRANCH=$PF_BRANCH" photoflow/docker-centos7-gtk bash #/sources/ci/appimage-centos7.sh
+docker run -it -v $(pwd):/sources -e "PF_BRANCH=$PF_BRANCH" -e "TRAVIS_BRANCH=$PF_BRANCH" photoflow/docker-centos7-gtk bash #/sources/ci/appimage-centos7.sh
