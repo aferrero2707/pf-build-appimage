fc0d9da38ee0c0cb441124d9a7f50641573406e6
aferrero2707 <aferrero1975@gmail.com>
2019-06-13 12:12:34 +0200

    Added standard environment initialisation in AppRun

--- AppRun.sh
+++ AppRun.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/usr/bin/env bash
 HERE="$(dirname "$(readlink -f "${0}")")"
 export APPDIR="$HERE"
 
@@ -16,15 +16,18 @@ fix_library "libfontconfig"
 fix_library "libfreetype"
 #exit
 
-export PATH="${PATH}:/sbin:/usr/sbin"
-export LD_LIBRARY_PATH="$AILIBDIR:/usr/lib:$LD_LIBRARY_PATH"
-export XDG_DATA_DIRS="${APPDIR}/usr/share/:${APPDIR}/usr/share/:${XDG_DATA_DIRS}: /usr/local/share/:/usr/share/"
-export GTK_PATH="${APPDIR}/usr/lib/gtk-2.0:${GTK_PATH}"
-export PANGO_LIBDIR="${APPDIR}/usr/lib"
-export GCONV_PATH="${APPDIR}/usr/lib/gconv"
+init_environment
+init_gtk
+
+#export PATH="${PATH}:/sbin:/usr/sbin"
+#export LD_LIBRARY_PATH="$AILIBDIR:/usr/lib:$LD_LIBRARY_PATH"
+#export XDG_DATA_DIRS="${APPDIR}/usr/share/:${APPDIR}/usr/share/:${XDG_DATA_DIRS}: /usr/local/share/:/usr/share/"
+#export GTK_PATH="${APPDIR}/usr/lib/gtk-2.0:${GTK_PATH}"
+#export PANGO_LIBDIR="${APPDIR}/usr/lib"
+#export GCONV_PATH="${APPDIR}/usr/lib/gconv"
+#export GDK_PIXBUF_MODULEDIR="${APPDIR}/usr/lib/gdk-pixbuf-2.0/loaders"
+#export GDK_PIXBUF_MODULE_FILE="${APPDIR}/usr/lib/gdk-pixbuf-2.0/loaders.cache"
 export PF_DATA_DIR="${APPDIR}/usr/share"
-export GDK_PIXBUF_MODULEDIR="${APPDIR}/usr/lib/gdk-pixbuf-2.0/loaders"
-export GDK_PIXBUF_MODULE_FILE="${APPDIR}/usr/lib/gdk-pixbuf-2.0/loaders.cache"
 
 export APPIMAGE_OWD=$(pwd)
 
